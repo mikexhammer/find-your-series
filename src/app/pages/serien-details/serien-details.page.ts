da8f34b903ffb17b5711a123a31bd1e8d459a399
mihammer <mike.hammer@hs-osnabrueck.de>
2023-02-22 14:22:32 +0100

    tutorial done

--- src/app/pages/serien-details/serien-details.page.ts
+++ src/app/pages/serien-details/serien-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SerieService } from 'src/app/services/serie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-serien-details',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./serien-details.page.scss'],
 })
 export class SerienDetailsPage implements OnInit {
+  serie: any;
+  imageBaseUrl = environment.images;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private serieService: SerieService
+  ) {}
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    this.serieService.getSerieDetails(id as string).subscribe((res) => {
+      console.log(res);
+      this.serie = res;
+    });
   }
 
+  openHomepage() {
+    window.open(this.serie);
+  }
 }
