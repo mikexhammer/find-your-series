5e165382ac687aecba0ff04c05e2129400175217
mihammer <mike.hammer@hs-osnabrueck.de>
2023-02-23 11:56:15 +0100

    messed up last commit

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { SerieService } from 'src/app/services/serie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.page.html',
   styleUrls: ['./favorites.page.scss'],
 })
+// https://ionicframework.com/docs/api/refresher
 export class FavoritesPage implements OnInit {
+  handlerMessage = '';
+  roleMessage = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  handleRefresh(event) {
+    setTimeout(() => {
+      this.setFavorite();
+      event.target.complete();
+    }, 1000);
   }
 
+  favorites = [] as any[];
+  imageBaseUrl = environment.images;
+
+  constructor(
+    private serieService: SerieService,
+    private alertController: AlertController
+  ) {}
+
+  ngOnInit() {
+    this.setFavorite();
+  }
+
+  setFavorite() {
+    this.favorites = this.serieService.getSeriesFromLocalStorage();
+  }
+
+  async presentAlert(serie) {
+    const alert = await this.alertController.create({
+      header: 'Sure you want to delete this series?',
+      buttons: [
+        {
+          text: 'No, sorry.',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'Yes, please!',
+          role: 'confirm',
+          handler: () => {
+            this.serieService.deleteSerieFromLocalStorage(serie);
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
+    this.setFavorite();
+  }
 }
